6392b110feaaec2995c1b0ddf488818a665e81ee
norrec99 <cagataysoyer@gmail.com>
2022-04-07 00:47:21 +0300

    update: switch to timeSinceLevelLoad

--- Assets/Scripts/MeteorSpawner.cs
+++ Assets/Scripts/MeteorSpawner.cs
@@ -25,34 +25,34 @@ public class MeteorSpawner : MonoBehaviour
 
   void SpawnHugeMeteor()
   {
-    if (Time.time - hugeMeteorLastSpawnTime >= Random.Range(8f, 15f))
+    if (Time.timeSinceLevelLoad - hugeMeteorLastSpawnTime >= Random.Range(8f, 15f))
     {
       Instantiate(hugeMeteor, new Vector3(hugeMeteor.transform.position.x + Random.Range(-2.5f, 2.5f), hugeMeteor.transform.position.y + 5f, hugeMeteor.transform.position.z), Quaternion.identity);
-      hugeMeteorLastSpawnTime = Time.time;
+      hugeMeteorLastSpawnTime = Time.timeSinceLevelLoad;
     }
   }
   void SpawnBigMeteor()
   {
-    if (Time.time - bigMeteorLastSpawnTime >= Random.Range(5f, 10f))
+    if (Time.timeSinceLevelLoad - bigMeteorLastSpawnTime >= Random.Range(5f, 10f))
     {
       Instantiate(bigMeteor, new Vector3(bigMeteor.transform.position.x + Random.Range(-2.5f, 2.5f), bigMeteor.transform.position.y + 5f, bigMeteor.transform.position.z), Quaternion.identity);
-      bigMeteorLastSpawnTime = Time.time;
+      bigMeteorLastSpawnTime = Time.timeSinceLevelLoad;
     }
   }
   void SpawnMedMeteor()
   {
-    if (Time.time - medMeteorLastSpawnTime >= Random.Range(2f, 6f))
+    if (Time.timeSinceLevelLoad - medMeteorLastSpawnTime >= Random.Range(2f, 6f))
     {
       Instantiate(medMeteor, new Vector3(medMeteor.transform.position.x + Random.Range(-2.5f, 2.5f), medMeteor.transform.position.y + 5f, medMeteor.transform.position.z), Quaternion.identity);
-      medMeteorLastSpawnTime = Time.time;
+      medMeteorLastSpawnTime = Time.timeSinceLevelLoad;
     }
   }
   void SpawnSmallMeteor()
   {
-    if (Time.time - smallMeteorLastSpawnTime >= Random.Range(1f, 5f))
+    if (Time.timeSinceLevelLoad - smallMeteorLastSpawnTime >= Random.Range(1f, 5f))
     {
       Instantiate(smallMeteor, new Vector3(smallMeteor.transform.position.x + Random.Range(-2.5f, 2.5f), smallMeteor.transform.position.y + 6f, smallMeteor.transform.position.z), Quaternion.identity);
-      smallMeteorLastSpawnTime = Time.time;
+      smallMeteorLastSpawnTime = Time.timeSinceLevelLoad;
     }
   }
 }
