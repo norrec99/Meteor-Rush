8f1be66fac12cab2f5f45b3395909a0bdc873a5c
norrec99 <cagataysoyer@gmail.com>
2022-04-07 00:09:58 +0300

    update: freeze time and reload level

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
@@ -13,7 +14,7 @@ public class CollisionHandler : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-
+    ReloadLevel();
   }
 
   void OnCollisionEnter2D(Collision2D other)
@@ -21,7 +22,17 @@ public class CollisionHandler : MonoBehaviour
     if (other.gameObject.tag == "Player")
     {
       Debug.Log(other.gameObject);
-      Destroy(other.gameObject);
+      // Destroy(other.gameObject);
+      Time.timeScale = 0f;
+    }
+  }
+
+  void ReloadLevel()
+  {
+    if (Time.timeScale == 0f && Input.touchCount > 1)
+    {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+      Time.timeScale = 1f;
     }
   }
 }
