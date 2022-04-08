2fde5f4e7c550d26bedd41f6195c867696cbf539
norrec99 <cagataysoyer@gmail.com>
2022-04-08 20:15:51 +0300

    update: extract score calculator

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,19 +10,15 @@ public class PlayerMovement : MonoBehaviour
   ScoreBoard scoreBoard;
 
   public float moveSpeed = 0.2f;
-  [SerializeField] float multiplier = 0.1f;
-  [SerializeField] float scorePerSecond = 5f;
-  float totalScore;
 
   void Start()
   {
-    scoreBoard = FindObjectOfType<ScoreBoard>();
+
   }
 
   void FixedUpdate()
   {
     TouchMove();
-    ScoreCalculator();
   }
 
   void TouchMove()
@@ -43,36 +39,4 @@ public class PlayerMovement : MonoBehaviour
       }
     }
   }
-
-  void ScoreCalculator()
-  {
-    if (Time.timeSinceLevelLoad < 5)
-    {
-      totalScore += scorePerSecond * multiplier * Time.deltaTime;
-      scoreBoard.IncreaseScore(totalScore);
-    }
-    else if (Time.timeSinceLevelLoad > 5)
-    {
-      totalScore += scorePerSecond * multiplier * Time.deltaTime;
-      scoreBoard.IncreaseScoreWithMultiplier(totalScore, 0.2f);
-    }
-    else if (Time.timeSinceLevelLoad > 15)
-    {
-      totalScore += scorePerSecond * multiplier * Time.deltaTime;
-      scoreBoard.IncreaseScoreWithMultiplier(totalScore, 0.3f);
-    }
-    else if (Time.timeSinceLevelLoad > 25)
-    {
-      totalScore += scorePerSecond * multiplier * Time.deltaTime;
-      scoreBoard.IncreaseScoreWithMultiplier(totalScore, 0.5f);
-    }
-  }
-
-  // void OnCollisionEnter2D(Collision2D other)
-  // {
-  //   if (other.gameObject.tag == "Meteor")
-  //   {
-  //     explosionParticle.Play();
-  //   }
-  // }
 }
